d2ca18c9932391df4d0747850ff8a2c850f8b3e7
tarasbiakin <115660593+tarasbiakin@users.noreply.github.com>
2022-11-20 12:23:30 +0300

    22

---
+++ 20/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве
+
+Console.Write("Введите координаты точки ХA: ");
+double xa = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координаты точки YA: ");
+double ya = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координаты точки ХB: ");
+double xb = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координаты точки YB: ");
+double yb = Convert.ToDouble(Console.ReadLine());
+
+
+double Lenght (double xa, double ya,double yb, double xb){
+    double res = Math.Sqrt((xa-xb)*(xa-xb)+(ya-yb)*(ya-yb));
+    return res;
+}
+
+double lenght = Lenght(xa,ya,xb,yb);
+double lenghtRound= Math.Round(lenght,2, MidpointRounding.ToZero);
+Console.WriteLine(lenghtRound);
+
---
+++ 22/Program.cs
@@ -0,0 +1,13 @@
+﻿//22 напишите програму, которая принимает на вход число N и выдает таблицу квадратов чисел от 1 до N.
+
+Console.WriteLine("Введите число N");
+int x = Convert.ToInt32(Console.ReadLine());
+
+void Kvadrat(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        Console.WriteLine($"{i}|{i * i}");
+    }
+}
+Kvadrat(x);
