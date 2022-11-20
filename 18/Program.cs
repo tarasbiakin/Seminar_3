66357b5ada55905de3bb00e85f04c15550f94f34
tarasbiakin <115660593+tarasbiakin@users.noreply.github.com>
2022-11-20 11:04:11 +0300

    18-стринг

--- 18/Program.cs
+++ 18/Program.cs
@@ -1,14 +1,14 @@
 ﻿//18. Напишите програму, которая по заданному номеру четверти показывает диапазон возможных координат точек в этой четверти(Х и У)
 
 Console.WriteLine("Укажите номер четверти");
-int qarter = Convert.ToInt32(Console.ReadLine());
-
+int qarter = (Console.ReadLine());
+//метод принимает строковую переменную и передает строковую
 string Range(int quarter1){
-    if(quarter1 ==1) return "x > 0 и y > 0";
-    if(quarter1 ==2) return "x < 0 и y > 0";
-    if(quarter1 ==3) return "x > 0 и y < 0";
-    if(quarter1 ==4) return "x < 0 и y < 0";
-    return"Указан неверный номер четверти";
+    if(quarter1 =="1") return "x > 0 и y > 0";
+    if(quarter1 =="2") return "x < 0 и y > 0";
+    if(quarter1 =="3") return "x > 0 и y < 0";
+    if(quarter1 =="4") return "x < 0 и y < 0";
+    return"Введены неверные данные";
 }
 string range = Range(qarter);
 
